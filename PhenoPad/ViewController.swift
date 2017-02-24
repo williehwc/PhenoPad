4831437cd771c41e5276194941e282f669054bfd
jixuan-wang <jixuan.cs@gmail.com>
2017-02-24 10:06:17 -0500

    Initial Commit

---
+++ PhenoPad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhenoPad
+//
+//  Created by Jixuan Wang on 2017-02-24.
+//  Copyright © 2017 CCM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
